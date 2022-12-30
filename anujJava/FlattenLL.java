e3da4736480662887d4bb5123b8b3f2dc6f36898
Megha Shyam <meghashyamkunukuntla@gmail.com>
2022-12-30 16:54:59 +0530

    commited all

---
+++ anujJava/FlattenLL.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    int data;
+    Node next;
+    Node down;
+    Node(int data){
+        this.data=data;
+    }
+}
+class FlattenLL{
+    public static void main(String args[])
+}
